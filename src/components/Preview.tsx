f8f18f0640d407e66ef2713cd7c7ab2ed192948b
Claudio Araujo <calloman123@gmail.com>
2023-06-26 23:10:28 -0600

    Syntax Highlight in preview

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { unified } from "unified";
 import remarkGfm from "remark-gfm";
-import remarkParse from "remark-parse";
-import remarkReact from "remark-react";
-import "github-markdown-css/github-markdown.css";
 import { useEditMode, Modes } from "../store";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { gruvboxDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 interface Props {
   doc: string;
@@ -12,18 +11,36 @@ interface Props {
 
 const Preview: React.FC<Props> = (props) => {
   const mode = useEditMode((state) => state.mode);
-  const md = unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkReact, React)
-    .processSync(props.doc).result;
+  const { doc } = props;
   return (
     <div
       className={`w-1/2 overflow-auto p-2 prose prose-gruvbox flex-grow ${
         mode === Modes.Edit ? "hidden" : "block"
       }`}
     >
-      {md}
+      <ReactMarkdown
+        children={doc}
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                children={String(children).replace(/\n$/, "")}
+                language={match[1]}
+                style={gruvboxDark}
+                PreTag="div"
+                customStyle={{ backgroundColor: "transparent" }}
+              />
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      />
     </div>
   );
 };
